81fa0629e31b6151f6b54fbe1ad589300f71b254
Julien <julienmulle@gmail.com>
2021-11-24 14:11:15 +0100

    ajout assets + dossier

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Navigation} from '@react-navigation/native';
 
 const App = () => {
   return (
---
+++ src/screens/MovieScreen.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const MovieScreen = () => {
+  return (
+    <View>
+      <Text>MovieScreen</Text>
+    </View>
+  );
+};
+
+export default MovieScreen;
