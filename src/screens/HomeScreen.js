91e95403d404c9580a4cba0c63d3a78fd17f35ec
Julien <julienmulle@gmail.com>
2021-11-24 15:47:33 +0100

    pimp my homeScreen header

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,38 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, ScrollView} from 'react-native';
+import {Text, ScrollView, StyleSheet, View} from 'react-native';
+import Colors from '../constant/Colors';
 
 const HomeScreen = () => {
   return (
-    <ScrollView>
-      <Text>HomeScreen</Text>
+    <ScrollView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}> Now Playing</Text>
+        <Text style={styles.headerSubTitle}> View All</Text>
+      </View>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.BASIC_BACKGROUND,
+  },
+  headerContainer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  headerTitle:{
+    fontSize: 28,
+  },
+  headerSubTitle:{
+    fontSize: 13,
+    color: Colors.ACTIVE,
+  },
+});
 
 export default HomeScreen;
