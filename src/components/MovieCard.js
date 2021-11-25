62b96f5565c71b5256fbee4492abc82be6e0743b
Julien <julienmulle@gmail.com>
2021-11-25 17:01:18 +0100

    ajout de react-native-vector

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Colors from '../constant/Colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const myIcon= <Icon name="heart" size={20} color="#f51637" style={{marginRight: 5}} />;
 
 const MovieCard = () => {
     return (
-        <View style={styles.container}>
+        <TouchableOpacity>
+            <View style={styles.container}>
             <Text> MovieCard</Text>
-        </View>
+            </View>
+            <View>
+                <Text style={styles.movieTitle}>l'ami du bon gout</Text>
+                <View style={styles.movieSubTitleContainer}>
+                    <Text style={styles.movieSubTitle}>origine defake</Text>                
+                <View style={styles.rowAndCenter}>
+                    {myIcon}
+                    <Text> 90%</Text>   
+                </View>
+                </View>
+            </View>
+        </TouchableOpacity>
     )
 }
 
@@ -18,7 +33,22 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         elevation: 5,
         marginVertical: 2,
-    }
+    },
+    movieTitle:{
+
+    },
+    movieSubTitleContainer:{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    movieSubTitle:{
+
+    },
+    rowAndCenter:{
+        flexDirection: "row",
+        alignItems: "center",
+    },
 })
 
 export default MovieCard;
