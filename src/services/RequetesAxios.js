e05db9478bd7ab9a7c31712c89dff846e9ee75eb
Julien <julienmulle@gmail.com>
2021-11-25 22:58:22 +0100

    requete tmdb_http_request

--- src/services/RequetesAxios.js
+++ src/services/RequetesAxios.js
@@ -6,3 +6,10 @@ import {
     TMDB_IMAGE_BASE_URL,
     ENDPOINTS,
 } from '../constant/Urls';
+
+const TMDB_HTTP_REQUEST = axios.create({
+    baseURL: TMDB_BASE_URL,
+    params: {
+       api_key : TMDB_API_KEY
+    }
+});
