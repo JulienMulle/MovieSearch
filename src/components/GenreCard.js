32b8118c811e9dd29cf92f5eb079c0c2851eded8
Julien <julienmulle@gmail.com>
2021-11-25 09:04:00 +0100

    creation component genreCard

---
+++ src/components/GenreCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const GenreCard = () => {
+    return (
+        <View style={styles.container}>
+            <Text>GenreCard</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
+
+export default GenreCard
