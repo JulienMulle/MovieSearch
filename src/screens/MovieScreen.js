d4ad5daed80e099c7642c3aedee7e24ef05e5228
Julien <julienmulle@gmail.com>
2021-11-29 13:08:48 +0100

    mise en forme de MovieScreen

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -1,22 +1,40 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ScrollView, Dimensions, Image} from 'react-native';
+
+//Components
+import Colors from '../constant/Colors';
+
+const {height, width} = Dimensions.get('screen');
+const setHeight = (h)=> (height/100) * h;
+const setWidth = (w)=> (width/100) * w;
 
 const MovieScreen = ({route,navigation}) => {
   const {movieId} = route.params;
 
   return (
-    <View style={styles.container}>
-      <Text>MovieScreen {movieId}</Text>
+    <ScrollView >
+    <View style={styles.moviePosterImageContainer}>     
+      <Image style={styles.moviePosterImage} resizeMode="cover"/>
     </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent:"center",
-    alignItems: "center",
+    backgroundColor: Colors.BASIC_BACKGROUND
+  },
+  moviePosterImageContainer:{
+    height: setHeight(35),
+    width: setWidth(145),
+  },
+  moviePosterImage: {
+    borderBottomRightRadius: 300,
+    borderBottomLeftRadius: 300,
+    height: setHeight(35),
+    width: setWidth(145),
   }
 })
 
